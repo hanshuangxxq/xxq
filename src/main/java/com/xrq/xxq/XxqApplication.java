c944bce6b5295c62b9fae0e607db54e8a6deec87
寒霜 <gangziwo620@163.com>
2026-06-05 14:05:54 +0800

    本项目的启动类

---
+++ src/main/java/com/xrq/xxq/XxqApplication.java
@@ -0,0 +1,13 @@
+package com.xrq.xxq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxqApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (XxqApplication.class, args);
+    }
+
+}
