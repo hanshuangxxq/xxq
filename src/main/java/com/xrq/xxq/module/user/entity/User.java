efe9d2790c90298ac8c16720e89050a2573a1b65
寒霜 <gangziwo620@163.com>
2026-06-05 14:07:20 +0800

    用户类，暂时只定义了这些

---
+++ src/main/java/com/xrq/xxq/module/user/entity/User.java
@@ -0,0 +1,37 @@
+package com.xrq.xxq.module.user.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import org.jspecify.annotations.NonNull;
+
+import java.time.LocalDateTime;
+
+/**
+ * @类名 User
+ * @Date 2026/6/5
+ *
+ */
+@Data
+@TableName("user")
+public class User {
+    @TableId (type = IdType.AUTO)
+    private Long id; //主键id
+    @NonNull
+    private String name; //用户名
+    private String password; //密码
+    private String email; //邮箱
+    private String phone; //手机号
+    private GenderEnum gender; //性别
+    private String avatar; //头像（URL）
+    private String description; //简介
+    private String role; //权限
+    private LocalDateTime lastLoginTime; //上次登录时间
+    private LocalDateTime createTime; //账号创建时间
+    private Integer status; //账号状态 0:正常 1:禁用
+    @TableLogic (value = "0", delval = "1")
+    private Boolean deleted; //逻辑删除
+
+}
