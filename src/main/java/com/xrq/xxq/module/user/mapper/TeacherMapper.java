03740c4a7e2d70ccf41ac4a15951308cfcc415a8
寒霜 <gangziwo620@163.com>
2026-06-22 13:30:18 +0800

    教师的mapper类

---
+++ src/main/java/com/xrq/xxq/module/user/mapper/TeacherMapper.java
@@ -0,0 +1,14 @@
+package com.xrq.xxq.module.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.user.entity.user.Teacher;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @类名 TeacherMapper
+ * @Date 2026/6/22
+ * @Description 老师Mapper
+ */
+@Mapper
+public interface TeacherMapper extends BaseMapper<Teacher> {
+}
