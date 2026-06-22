949f9d8e427ffb348a7ce8cff56af2204b6607ce
寒霜 <gangziwo620@163.com>
2026-06-22 11:20:59 +0800

    添加了关于微信登录的方式

---
+++ src/main/java/com/xrq/xxq/module/user/entity/WXUser.java
@@ -0,0 +1,21 @@
+package com.xrq.xxq.module.user.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * @类名 WXUser
+ * @Date 2026/6/22
+ *
+ */
+@Data
+@TableName("wx_user")
+public class WXUser {
+    @TableId(type = IdType.AUTO)
+    private Long id; //主键id
+    private String userId; //关联的用户id
+    private String wxOpenid; //微信openid
+    private String wxUnionid; //微信unionid
+}
