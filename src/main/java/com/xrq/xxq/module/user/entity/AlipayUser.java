4631735008fddeae4e77f21b560521aeca12d260
寒霜 <gangziwo620@163.com>
2026-06-22 13:11:31 +0800

    统一注释缩进

--- src/main/java/com/xrq/xxq/module/user/entity/AlipayUser.java
+++ src/main/java/com/xrq/xxq/module/user/entity/AlipayUser.java
@@ -14,8 +14,8 @@ import lombok.Data;
 @TableName("alipay_user")
 public class AlipayUser {
     @TableId(type = IdType.AUTO)
-    private Long id;           // 主键id
-    private Long userId;       // 关联的用户表主键id
-    private String userType;   // 用户类型（teacher/student/dean）
-    private String alipayUserId;// 支付宝user_id
+    private Long id;             // 主键id
+    private Long userId;         // 关联的用户表主键id
+    private String userType;     // 用户类型（teacher/student/dean）
+    private String alipayUserId; // 支付宝user_id
 }
--- src/main/java/com/xrq/xxq/module/user/entity/WXUser.java
+++ src/main/java/com/xrq/xxq/module/user/entity/WXUser.java
@@ -14,9 +14,9 @@ import lombok.Data;
 @TableName("wx_user")
 public class WXUser {
     @TableId(type = IdType.AUTO)
-    private Long id;         // 主键id
-    private Long userId;     // 关联的用户表主键id
-    private String userType; // 用户类型（teacher/student/dean）
-    private String wxOpenid; // 微信openid
-    private String wxUnionid;// 微信unionid
+    private Long id;          // 主键id
+    private Long userId;      // 关联的用户表主键id
+    private String userType;  // 用户类型（teacher/student/dean）
+    private String wxOpenid;  // 微信openid
+    private String wxUnionid; // 微信unionid
 }
