0b0a2c25d4e2f9b969a4f2b30c551470dfcaca7c
寒霜 <gangziwo620@163.com>
2026-06-22 12:45:31 +0800

    修改了注释的位置

--- src/main/java/com/xrq/xxq/module/user/entity/User.java
+++ src/main/java/com/xrq/xxq/module/user/entity/User.java
@@ -18,20 +18,20 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 public abstract class User {
     @TableId (type = IdType.AUTO)
-    private Long id; //主键id
+    private Long id;                         //主键id
     @NonNull
-    private String name; //用户名
-    private String password; //密码
-    private String email; //邮箱
-    private String phone; //手机号
-    private GenderEnum gender; //性别
-    private String avatar; //头像（URL）
-    private String description; //简介
-    private String role; //权限
-    private LocalDateTime lastLoginTime; //上次登录时间
-    private LocalDateTime createTime; //账号创建时间
-    private Integer status; //账号状态 0:正常 1:禁用
+    private String name;                     //用户名
+    private String password;                 //密码
+    private String email;                    //邮箱
+    private String phone;                    //手机号
+    private GenderEnum gender;               //性别
+    private String avatar;                   //头像（URL）
+    private String description;              //简介
+    private String role;                     //权限
+    private LocalDateTime lastLoginTime;     //上次登录时间
+    private LocalDateTime createTime;        //账号创建时间
+    private Integer status;                  //账号状态 0:正常 1:禁用
     @TableLogic (value = "0", delval = "1")
-    private Boolean deleted; //逻辑删除
+    private Boolean deleted;                 //逻辑删除
 
 }
