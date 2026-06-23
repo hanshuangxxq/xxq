1d4590a7510049b94a2f3bb51f202d3600a435c6
寒霜 <gangziwo620@163.com>
2026-06-23 11:01:25 +0800

    添加了适当的注释

--- src/main/java/com/xrq/xxq/module/user/entity/GenderEnum.java
+++ src/main/java/com/xrq/xxq/module/user/entity/GenderEnum.java
@@ -4,22 +4,26 @@ import com.baomidou.mybatisplus.annotation.EnumValue;
 import lombok.Getter;
 
 /**
+ * 性别枚举。
+ * 通过 {@link EnumValue} 标记的 {@code code} 字段与数据库交互，MyBatis Plus 自动映射。
+ *
  * @类名 GenderEnum
  * @Date 2026/6/5
- *
  */
 @Getter
 public enum GenderEnum {
     MALE(0, "未知"),
     FEMALE(1, "男"),
     UNKNOWN(2, "女");
+
+    /** 数据库存取值 */
     @EnumValue
     private final int code;
+    /** 中文描述 */
     private final String desc;
 
     GenderEnum(int code, String desc) {
         this.code = code;
         this.desc = desc;
     }
-
 }
