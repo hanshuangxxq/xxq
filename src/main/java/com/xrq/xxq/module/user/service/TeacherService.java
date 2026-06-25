80905bf18e2aa0cc6f53b244218d768123dcd692
寒霜 <gangziwo620@163.com>
2026-06-25 12:55:56 +0800

    feat(user): add TeacherService interface
    Extend MyBatis Plus IService<Teacher> to provide generic CRUD
    operations for the teacher entity.

---
+++ src/main/java/com/xrq/xxq/module/user/service/TeacherService.java
@@ -0,0 +1,13 @@
+package com.xrq.xxq.module.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xrq.xxq.module.user.entity.user.Teacher;
+
+/**
+ * 教师业务服务，继承 MyBatis Plus IService 提供通用 CRUD。
+ *
+ * @类名 TeacherService
+ * @Date 2026/6/22
+ */
+public interface TeacherService extends IService<Teacher> {
+}
