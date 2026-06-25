081cde081da15fd15330a4d1a76f3cbfa3378870
寒霜 <gangziwo620@163.com>
2026-06-25 12:55:07 +0800

    feat(user): add StudentService interface extending MyBatis Plus IService
    Define StudentService interface for the student domain, extending
    IService<Student> to provide generic CRUD operations via MyBatis Plus.

---
+++ src/main/java/com/xrq/xxq/module/user/service/StudentService.java
@@ -0,0 +1,13 @@
+package com.xrq.xxq.module.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xrq.xxq.module.user.entity.user.Student;
+
+/**
+ * 学生业务服务，继承 MyBatis Plus IService 提供通用 CRUD。
+ *
+ * @类名 StudentService
+ * @Date 2026/6/22
+ */
+public interface StudentService extends IService<Student> {
+}
