9b0c579272ee7ae921ad0deb1e89e0a5994b4f64
寒霜 <gangziwo620@163.com>
2026-07-01 14:14:00 +0800

    feat(course): 添加Local实体的Mapper接口
    - 创建LocalMapper接口继承BaseMapper
    - 添加@Mapper注解以支持MyBatis扫描
    - 定义包路径为com.xrq.xxq.module.course.mapper
    - 为空的Mapper接口提供基本CRUD功能

---
+++ src/main/java/com/xrq/xxq/module/course/mapper/LocalMapper.java
@@ -0,0 +1,9 @@
+package com.xrq.xxq.module.course.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.course.entity.Local;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface LocalMapper extends BaseMapper<Local> {
+}
