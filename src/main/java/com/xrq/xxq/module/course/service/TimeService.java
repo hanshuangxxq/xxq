d00c7a8f773bc14aaa3083f93f3720022d0df8db
寒霜 <gangziwo620@163.com>
2026-07-01 14:15:12 +0800

    feat(course): 添加 TimeService 接口
    - 创建 TimeService 接口继承 IService<Time>
    - 为课程模块增加时间相关服务接口定义
    - 提供时间实体的基础服务能力扩展机会

---
+++ src/main/java/com/xrq/xxq/module/course/service/TimeService.java
@@ -0,0 +1,7 @@
+package com.xrq.xxq.module.course.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xrq.xxq.module.course.entity.Time;
+
+public interface TimeService extends IService<Time> {
+}
