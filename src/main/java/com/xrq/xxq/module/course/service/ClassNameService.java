51de2184a1fe537fbf5c17efd306f5e96864fc10
寒霜 <gangziwo620@163.com>
2026-07-01 14:13:17 +0800

    feat(course): 添加 ClassNameService 接口定义
    - 创建 ClassNameService 接口继承自 IService<ClassName>
    - 定义课程模块中班级名称相关的服务接口
    - 为后续实现班级名称业务逻辑提供基础接口

---
+++ src/main/java/com/xrq/xxq/module/course/service/ClassNameService.java
@@ -0,0 +1,7 @@
+package com.xrq.xxq.module.course.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xrq.xxq.module.course.entity.ClassName;
+
+public interface ClassNameService extends IService<ClassName> {
+}
