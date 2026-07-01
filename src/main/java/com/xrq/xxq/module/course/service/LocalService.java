4a758837c5cc4609c45cdb3493964a40bdfedeb2
寒霜 <gangziwo620@163.com>
2026-07-01 14:14:07 +0800

    feat(course): 添加 LocalService 接口定义
    - 创建 LocalService 接口继承 MyBatis-Plus 的 IService
    - 定义 Local 实体的业务操作规范
    - 为课程模块引入本地服务接口结构

---
+++ src/main/java/com/xrq/xxq/module/course/service/LocalService.java
@@ -0,0 +1,7 @@
+package com.xrq.xxq.module.course.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xrq.xxq.module.course.entity.Local;
+
+public interface LocalService extends IService<Local> {
+}
