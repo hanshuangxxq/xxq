8275898cda1b4258b4a38634e25211a8419e7686
寒霜 <gangziwo620@163.com>
2026-07-02 14:45:09 +0800

    feat(course): 添加TimeRestriction数据访问接口
    - 创建TimeRestrictionMapper接口继承BaseMapper
    - 添加@Mapper注解支持MyBatis映射
    - 便于数据库操作TimeRestriction实体数据
    - 初始化课程时间限制模块的数据层组件

---
+++ src/main/java/com/xrq/xxq/module/course/mapper/TimeRestrictionMapper.java
@@ -0,0 +1,9 @@
+package com.xrq.xxq.module.course.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.course.entity.TimeRestriction;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TimeRestrictionMapper extends BaseMapper<TimeRestriction> {
+}
