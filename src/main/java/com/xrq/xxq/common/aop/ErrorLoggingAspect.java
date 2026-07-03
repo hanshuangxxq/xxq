9ea55f7d9a7b55731a1bc5796c3544c22387d528
寒霜 <gangziwo620@163.com>
2026-07-03 09:24:32 +0800

    feat(aop): 添加异常日志去重及范围控制功能
    - 引入ScopedValue实现异常日志记录的范围控制
    - 在controller层方法切面中建立ScopedValue作用域，避免重复记录同一异常
    - 在异常日志切面中判断异常是否已记录，防止重复日志输出
    - 限定日志切点为module模块及controller子模块方法
    - 使用Set存储已记录异常的identityHashCode，实现异常对象去重机制

--- src/main/java/com/xrq/xxq/common/aop/ErrorLoggingAspect.java
+++ src/main/java/com/xrq/xxq/common/aop/ErrorLoggingAspect.java
@@ -3,21 +3,43 @@ package com.xrq.xxq.common.aop;
 import com.xrq.xxq.common.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Aspect
 @Component
 @Slf4j
 public class ErrorLoggingAspect {
 
+    private static final ScopedValue<Set<Integer>> LOGGED_EXCEPTIONS = ScopedValue.newInstance();
+
+    @Pointcut("execution(* com.xrq.xxq.module..controller..*.*(..))")
+    public void controllerMethods() {}
+
     @Pointcut("execution(* com.xrq.xxq.module..*.*(..))")
     public void moduleMethods() {}
 
+    @Around("controllerMethods()")
+    public Object establishScope(ProceedingJoinPoint pjp) throws Throwable {
+        return ScopedValue.where(LOGGED_EXCEPTIONS, new HashSet<>()).call(pjp::proceed);
+    }
+
     @AfterThrowing(pointcut = "moduleMethods()", throwing = "e")
     public void logException(JoinPoint joinPoint, Exception e) {
+        if (LOGGED_EXCEPTIONS.isBound()) {
+            Set<Integer> logged = LOGGED_EXCEPTIONS.get();
+            if (!logged.add(System.identityHashCode(e))) {
+                return;
+            }
+        }
+
         String className = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
 
