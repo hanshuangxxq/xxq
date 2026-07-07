84af0b7179f97197b5fa376e4caec2734db1a165
寒霜 <gangziwo620@163.com>
2026-07-07 09:40:54 +0800

    feat(user): 添加 MajorService 接口定义
    - 新增 MajorService 接口，继承 IService<Major>
    - 作为用户模块中 Major 实体的服务层接口
    - 预留扩展 Major 相关业务逻辑的接口位置

---
+++ src/main/java/com/xrq/xxq/module/user/service/MajorService.java
@@ -0,0 +1,7 @@
+package com.xrq.xxq.module.user.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xrq.xxq.module.user.entity.Major;
+
+public interface MajorService extends IService<Major> {
+}
