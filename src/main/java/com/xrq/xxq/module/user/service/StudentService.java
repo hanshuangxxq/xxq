d41d511021b08e5a80260567be08d48f84882c44
寒霜 <gangziwo620@163.com>
2026-07-07 09:40:10 +0800

    feat(user): 添加学生信息查询与更新接口
    - 新增 queryStudents 方法用于多条件查询学生信息
    - 新增 updateStudentInfo 方法支持学生信息更新
    - 引入 StudentDto 和 UpdateStudentRequest 数据传输对象
    - 扩展 StudentService 接口以支持新增功能

--- src/main/java/com/xrq/xxq/module/user/service/StudentService.java
+++ src/main/java/com/xrq/xxq/module/user/service/StudentService.java
@@ -1,7 +1,11 @@
 package com.xrq.xxq.module.user.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.xrq.xxq.module.user.dto.StudentDto;
+import com.xrq.xxq.module.user.dto.UpdateStudentRequest;
 import com.xrq.xxq.module.user.entity.user.Student;
+
+import java.util.List;
 
 /**
  * 学生业务服务，继承 MyBatis Plus IService 提供通用 CRUD。
@@ -10,4 +14,8 @@ import com.xrq.xxq.module.user.entity.user.Student;
  * @Date 2026/6/22
  */
 public interface StudentService extends IService<Student> {
+
+    List<StudentDto> queryStudents(String grade, List<Long> classIds, List<Long> majorIds, Boolean unassigned, String name);
+
+    boolean updateStudentInfo(Long studentId, UpdateStudentRequest request);
 }
