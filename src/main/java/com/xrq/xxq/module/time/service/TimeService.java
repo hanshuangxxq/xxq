05d30a9f0843e7831d7b3a27c0dec709a7dab8b2
寒霜 <gangziwo620@163.com>
2026-07-09 11:45:41 +0800

    refactor(time): 重命名TimeService到time模块
    - 将TimeService接口从course模块移动到time模块
    - 修改包路径为com.xrq.xxq.module.time.service
    - 更新相关Time实体的包路径到time模块
    - 保持接口内容和功能不变

--- src/main/java/com/xrq/xxq/module/time/service/TimeService.java
+++ src/main/java/com/xrq/xxq/module/time/service/TimeService.java
@@ -1,7 +1,7 @@
-package com.xrq.xxq.module.course.service;
+package com.xrq.xxq.module.time.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.xrq.xxq.module.course.entity.Time;
+import com.xrq.xxq.module.time.entity.Time;
 
 public interface TimeService extends IService<Time> {
 }
