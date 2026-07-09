e17ccb72cf0672ba6c979bea6df3cacbc97b8118
寒霜 <gangziwo620@163.com>
2026-07-09 11:45:48 +0800

    refactor(time): 重构 TimeServiceImpl 类路径和包名
    - 将 TimeServiceImpl 类从 course 模块移动到 time 模块
    - 更新包声明为 com.xrq.xxq.module.time.service.impl
    - 更新实体、映射器及服务接口的导入路径以匹配新包结构
    - 保持类内容和功能不变，仅调整包结构以优化模块划分

--- src/main/java/com/xrq/xxq/module/time/service/impl/TimeServiceImpl.java
+++ src/main/java/com/xrq/xxq/module/time/service/impl/TimeServiceImpl.java
@@ -1,9 +1,9 @@
-package com.xrq.xxq.module.course.service.impl;
+package com.xrq.xxq.module.time.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.xrq.xxq.module.course.entity.Time;
-import com.xrq.xxq.module.course.mapper.TimeMapper;
-import com.xrq.xxq.module.course.service.TimeService;
+import com.xrq.xxq.module.time.entity.Time;
+import com.xrq.xxq.module.time.mapper.TimeMapper;
+import com.xrq.xxq.module.time.service.TimeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
