5f6188c73423a7630d9257adf29130bed7fc2d1f
寒霜 <gangziwo620@163.com>
2026-07-09 11:45:35 +0800

    refactor(time): 重构TimeRestrictionServiceImpl模块路径
    - 将TimeRestrictionServiceImpl类从course模块迁移到time模块
    - 更新包路径以匹配新的模块结构
    - 修改相关import语句指向time模块下的实体、映射和服务接口
    - 保持类实现和服务注解不变，确保功能一致

--- src/main/java/com/xrq/xxq/module/time/service/impl/TimeRestrictionServiceImpl.java
+++ src/main/java/com/xrq/xxq/module/time/service/impl/TimeRestrictionServiceImpl.java
@@ -1,9 +1,9 @@
-package com.xrq.xxq.module.course.service.impl;
+package com.xrq.xxq.module.time.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.xrq.xxq.module.course.entity.TimeRestriction;
-import com.xrq.xxq.module.course.mapper.TimeRestrictionMapper;
-import com.xrq.xxq.module.course.service.TimeRestrictionService;
+import com.xrq.xxq.module.time.entity.TimeRestriction;
+import com.xrq.xxq.module.time.mapper.TimeRestrictionMapper;
+import com.xrq.xxq.module.time.service.TimeRestrictionService;
 import org.springframework.stereotype.Service;
 
 @Service
