19806bd44c7c4e22e29a5d00e71eef9d66d28bf6
寒霜 <gangziwo620@163.com>
2026-07-09 11:38:51 +0800

    refactor(clazz): 重命名ClassNameServiceImpl所在包路径
    - 将ClassNameServiceImpl类从course包移动到clazz包
    - 修改ClassNameServiceImpl文件的包声明及相关导入路径
    - 保持类功能不变，仅调整模块组织结构
    - 统一clazz模块下的实体、Mapper和Service路径

--- src/main/java/com/xrq/xxq/module/clazz/service/impl/ClassNameServiceImpl.java
+++ src/main/java/com/xrq/xxq/module/clazz/service/impl/ClassNameServiceImpl.java
@@ -1,9 +1,9 @@
-package com.xrq.xxq.module.course.service.impl;
+package com.xrq.xxq.module.clazz.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.xrq.xxq.module.course.entity.ClassName;
-import com.xrq.xxq.module.course.mapper.ClassNameMapper;
-import com.xrq.xxq.module.course.service.ClassNameService;
+import com.xrq.xxq.module.clazz.entity.ClassName;
+import com.xrq.xxq.module.clazz.mapper.ClassNameMapper;
+import com.xrq.xxq.module.clazz.service.ClassNameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
