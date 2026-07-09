15b5f7a7b7000b11ada9f82490cd9e53a92e2eb3
寒霜 <gangziwo620@163.com>
2026-07-09 13:32:02 +0800

    feat(selection): 新增5个Mapper接口

---
+++ src/main/java/com/xrq/xxq/module/selection/mapper/SelectionCampaignMapper.java
@@ -0,0 +1,10 @@
+package com.xrq.xxq.module.selection.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.selection.entity.SelectionCampaign;
+
+@Mapper
+public interface SelectionCampaignMapper extends BaseMapper<SelectionCampaign> {
+}
---
+++ src/main/java/com/xrq/xxq/module/selection/mapper/SelectionRecordMapper.java
@@ -0,0 +1,10 @@
+package com.xrq.xxq.module.selection.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.selection.entity.SelectionRecord;
+
+@Mapper
+public interface SelectionRecordMapper extends BaseMapper<SelectionRecord> {
+}
