9f1ab8bef4b9793cea8ccc0728266c9ee0df9d9a
寒霜 <gangziwo620@163.com>
2026-07-09 12:44:02 +0800

    refactor(user): 更换IService接口的导入路径
    - 将MyBatis-Plus的IService接口导入从extension.service改为spring.service
    - 优化了服务类依赖，提升兼容性和维护性

--- src/main/java/com/xrq/xxq/module/user/service/UserService.java
+++ src/main/java/com/xrq/xxq/module/user/service/UserService.java
@@ -1,6 +1,6 @@
 package com.xrq.xxq.module.user.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.spring.service.IService;
 import com.xrq.xxq.module.user.dto.UpdateProfileRequest;
 import com.xrq.xxq.module.user.dto.UserProfileResponse;
 import com.xrq.xxq.module.user.entity.User;
