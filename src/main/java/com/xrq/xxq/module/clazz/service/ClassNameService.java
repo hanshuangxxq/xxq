b025368a32361f38adfb2cd052ed1c09d71f08ec
寒霜 <gangziwo620@163.com>
2026-07-09 11:38:43 +0800

    refactor(clazz): 重构ClassNameService包路径
    - 将ClassNameService从course模块移动到clazz模块
    - 更新了ClassNameService的包名声明
    - 修改了ClassName实体类的导入路径对应新模块

--- src/main/java/com/xrq/xxq/module/clazz/service/ClassNameService.java
+++ src/main/java/com/xrq/xxq/module/clazz/service/ClassNameService.java
@@ -1,7 +1,7 @@
-package com.xrq.xxq.module.course.service;
+package com.xrq.xxq.module.clazz.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.xrq.xxq.module.course.entity.ClassName;
+import com.xrq.xxq.module.clazz.entity.ClassName;
 
 public interface ClassNameService extends IService<ClassName> {
 }
