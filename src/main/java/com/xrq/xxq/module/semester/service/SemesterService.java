f754e2197dc22b970fe6df784f28c3635c75859a
寒霜 <gangziwo620@163.com>
2026-07-09 11:43:35 +0800

    refactor(semester): 将 SemesterService 移动到 semester 模块
    - 重命名 SemesterService 文件位置，从 course 模块迁移至 semester 模块
    - 修改 SemesterService 包路径以匹配新的模块结构
    - 更新 Semester 类的导入路径以对应新的包位置

--- src/main/java/com/xrq/xxq/module/semester/service/SemesterService.java
+++ src/main/java/com/xrq/xxq/module/semester/service/SemesterService.java
@@ -1,7 +1,7 @@
-package com.xrq.xxq.module.course.service;
+package com.xrq.xxq.module.semester.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.xrq.xxq.module.course.entity.Semester;
+import com.xrq.xxq.module.semester.entity.Semester;
 
 public interface SemesterService extends IService<Semester> {
     Semester getCurrent();
