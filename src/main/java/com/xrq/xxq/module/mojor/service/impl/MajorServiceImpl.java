59260c632a2b74df7948ea3915f9fccfb95b8d1d
寒霜 <gangziwo620@163.com>
2026-07-09 11:40:33 +0800

    refactor(mojor): 重命名模块目录和包路径
    - 将模块包名从 user 修改为 mojor
    - 移动 MajorServiceImpl 文件到新的包路径
    - 更新相关导入路径以匹配新的包结构

--- src/main/java/com/xrq/xxq/module/mojor/service/impl/MajorServiceImpl.java
+++ src/main/java/com/xrq/xxq/module/mojor/service/impl/MajorServiceImpl.java
@@ -1,9 +1,9 @@
-package com.xrq.xxq.module.user.service.impl;
+package com.xrq.xxq.module.mojor.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.xrq.xxq.module.user.entity.Major;
-import com.xrq.xxq.module.user.mapper.MajorMapper;
-import com.xrq.xxq.module.user.service.MajorService;
+import com.xrq.xxq.module.mojor.entity.Major;
+import com.xrq.xxq.module.mojor.mapper.MajorMapper;
+import com.xrq.xxq.module.mojor.service.MajorService;
 import org.springframework.stereotype.Service;
 
 @Service
