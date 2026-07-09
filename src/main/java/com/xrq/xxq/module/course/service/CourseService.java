e6c4ff07b15e243fe095db8bbe5f1ab883e33c8b
寒霜 <gangziwo620@163.com>
2026-07-09 12:52:01 +0800

    refactor(course): 替换导入的IService接口路径
    - 将com.baomidou.mybatisplus.extension.service.IService改为com.baomidou.mybatisplus.spring.service.IService
    - 更新CourseService中的IService接口引用
    - 统一依赖注入接口的版本和路径规范

--- src/main/java/com/xrq/xxq/module/course/service/CourseService.java
+++ src/main/java/com/xrq/xxq/module/course/service/CourseService.java
@@ -1,6 +1,6 @@
 package com.xrq.xxq.module.course.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.spring.service.IService;
 import com.xrq.xxq.module.course.entity.Course;
 
 /**
