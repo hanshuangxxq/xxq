278cf1a9376518b230f1083ebe72f05306519095
寒霜 <gangziwo620@163.com>
2026-07-09 11:45:29 +0800

    refactor(time): 重构时段限制服务包路径
    - 将 TimeRestrictionService 类从 course 包移动到 time 包
    - 更新包声明以匹配新的包路径
    - 修改实体类导入路径以对应 time 包中的 TimeRestriction

--- src/main/java/com/xrq/xxq/module/time/service/TimeRestrictionService.java
+++ src/main/java/com/xrq/xxq/module/time/service/TimeRestrictionService.java
@@ -1,7 +1,7 @@
-package com.xrq.xxq.module.course.service;
+package com.xrq.xxq.module.time.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.xrq.xxq.module.course.entity.TimeRestriction;
+import com.xrq.xxq.module.time.entity.TimeRestriction;
 
 /**
  * 时段限制规则管理服务。
