682dc7c27db1424147809bf98e347f5a4aad2c97
寒霜 <gangziwo620@163.com>
2026-07-09 12:45:10 +0800

    fix(time): 修正ServiceImpl导入路径错误
    - 将ServiceImpl的导入路径从mybatis-plus.extension修改为mybatis-plus.spring
    - 修正包引用以匹配正确的类位置
    - 避免因路径错误导致的编译问题

--- src/main/java/com/xrq/xxq/module/time/service/impl/TimeServiceImpl.java
+++ src/main/java/com/xrq/xxq/module/time/service/impl/TimeServiceImpl.java
@@ -1,6 +1,6 @@
 package com.xrq.xxq.module.time.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.baomidou.mybatisplus.spring.service.impl.ServiceImpl;
 import com.xrq.xxq.module.time.entity.Time;
 import com.xrq.xxq.module.time.mapper.TimeMapper;
 import com.xrq.xxq.module.time.service.TimeService;
