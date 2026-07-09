8274697dd425b40939a1785d3f3ebea01a2bac27
寒霜 <gangziwo620@163.com>
2026-07-09 12:50:10 +0800

    refactor(service): 修正 MajorService 中 IService 引入路径
    - 将 IService 的导入路径由 extension.service 更正为 spring.service
    - 确保依赖路径正确以避免潜在兼容性问题
    - 优化代码依赖管理，提高代码稳定性

--- src/main/java/com/xrq/xxq/module/mojor/service/MajorService.java
+++ src/main/java/com/xrq/xxq/module/mojor/service/MajorService.java
@@ -1,6 +1,6 @@
 package com.xrq.xxq.module.mojor.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.spring.service.IService;
 import com.xrq.xxq.module.mojor.entity.Major;
 
 public interface MajorService extends IService<Major> {
