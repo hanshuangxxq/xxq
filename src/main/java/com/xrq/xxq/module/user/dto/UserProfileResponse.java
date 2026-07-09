56dfcd3117eea0b837c61d9501ec983675a56770
寒霜 <gangziwo620@163.com>
2026-07-09 11:46:15 +0800

    refactor(user): 调整UserProfileResponse中ClassName的导入路径
    - 将ClassName的导入路径从module.course.entity修改为module.clazz.entity
    - 为UserProfileResponse类添加类级注释描述
    - 保持代码结构整洁，提高代码可读性和规范性

--- src/main/java/com/xrq/xxq/module/user/dto/UserProfileResponse.java
+++ src/main/java/com/xrq/xxq/module/user/dto/UserProfileResponse.java
@@ -1,11 +1,14 @@
 package com.xrq.xxq.module.user.dto;
 
-import com.xrq.xxq.module.course.entity.ClassName;
+import com.xrq.xxq.module.clazz.entity.ClassName;
 import com.xrq.xxq.module.user.entity.GenderEnum;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
+/**
+ * 用户资料
+ */
 @Data
 public class UserProfileResponse {
     private Long userId;
