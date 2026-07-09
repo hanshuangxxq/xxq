0ffd3716d4689dab655f707c05ed8bd0a848f2b3
寒霜 <gangziwo620@163.com>
2026-07-09 12:51:56 +0800

    fix(course): 修正MyBatis-Plus ServiceImpl导入路径
    - 将ServiceImpl的导入路径从`com.baomidou.mybatisplus.extension.service.impl`修改为`com.baomidou.mybatisplus.spring.service.impl`

--- src/main/java/com/xrq/xxq/module/course/service/impl/CourseServiceImpl.java
+++ src/main/java/com/xrq/xxq/module/course/service/impl/CourseServiceImpl.java
@@ -1,6 +1,6 @@
 package com.xrq.xxq.module.course.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.baomidou.mybatisplus.spring.service.impl.ServiceImpl;
 import com.xrq.xxq.module.course.entity.Course;
 import com.xrq.xxq.module.course.mapper.CourseMapper;
 import com.xrq.xxq.module.course.service.CourseService;
