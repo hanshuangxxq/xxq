e8bf312396cbfa2ecc121c0aafd07b7aa50fbaa3
寒霜 <gangziwo620@163.com>
2026-07-25 16:13:48 +0800

    feat(user): 添加 GradeService 接口定义
    - 新增了 GradeService 接口，继承自 IService<Grade>
    - 定义了用户等级相关的服务契约
    - 为后续用户等级逻辑实现提供基础接口

---
+++ src/main/java/com/xrq/xxq/module/user/service/GradeService.java
@@ -0,0 +1,7 @@
+package com.xrq.xxq.module.user.service;
+
+import com.baomidou.mybatisplus.spring.service.IService;
+import com.xrq.xxq.module.user.entity.user.Grade;
+
+public interface GradeService extends IService<Grade> {
+}
