1a7eec3e954be0541c85832bd6e05df781ca12ae
寒霜 <gangziwo620@163.com>
2026-07-25 16:15:16 +0800

    feat(selection): 添加选课组创建请求数据传输对象
    - 新增SelectionGroupCreateRequest类
    - 包含name和maxCourses的非空字段
    - 添加了可选的sortOrder字段
    - 使用Lombok注解简化代码编写

---
+++ src/main/java/com/xrq/xxq/module/selection/dto/SelectionGroupCreateRequest.java
@@ -0,0 +1,14 @@
+package com.xrq.xxq.module.selection.dto;
+
+import org.jspecify.annotations.NonNull;
+
+import lombok.Data;
+
+@Data
+public class SelectionGroupCreateRequest {
+    @NonNull
+    private String name;
+    @NonNull
+    private Integer maxCourses;
+    private Integer sortOrder;
+}
