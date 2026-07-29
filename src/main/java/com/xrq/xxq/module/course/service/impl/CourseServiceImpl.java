8d6eb5a79911a130ab38bde227b4a11c68eb862b
寒霜 <gangziwo620@163.com>
2026-07-29 23:18:34 +0800

    feat(course): 优化删除课程时的联动删除逻辑
    - 引入事务管理确保删除操作的原子性
    - 在删除课程前删除对应的选课活动数据
    - 增加对 SelectionCampaignService 的依赖注入
    - 重写 removeById 方法实现联动删除功能

--- src/main/java/com/xrq/xxq/module/course/service/impl/CourseServiceImpl.java
+++ src/main/java/com/xrq/xxq/module/course/service/impl/CourseServiceImpl.java
@@ -1,11 +1,17 @@
 package com.xrq.xxq.module.course.service.impl;
 
+import java.io.Serializable;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.baomidou.mybatisplus.spring.service.impl.ServiceImpl;
 import com.xrq.xxq.module.course.entity.Course;
 import com.xrq.xxq.module.course.mapper.CourseMapper;
 import com.xrq.xxq.module.course.service.CourseService;
+import com.xrq.xxq.module.selection.service.SelectionCampaignService;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 /**
  * 课程服务实现，继承 MyBatis Plus ServiceImpl 提供通用 CRUD。
@@ -17,4 +23,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CourseServiceImpl extends ServiceImpl<CourseMapper, Course> implements CourseService {
     private final CourseMapper courseMapper;
+    private final SelectionCampaignService selectionCampaignService;
+
+    @Override
+    @Transactional
+    public boolean removeById(Serializable id) {
+        selectionCampaignService.deleteByCourseId((Long) id);
+        return super.removeById(id);
+    }
 }
