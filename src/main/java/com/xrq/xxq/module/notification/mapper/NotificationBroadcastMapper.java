494d05922a083becd6b065ceba133e0be006ffc8
寒霜 <gangziwo620@163.com>
2026-07-30 21:09:59 +0800

    feat(notification): 添加广播通知数据访问接口
    - 创建 NotificationBroadcastMapper 接口
    - 扩展 MyBatis-Plus BaseMapper 以支持 CRUD 操作
    - 使用 @Mapper 注解集成 MyBatis 框架
    - 提供广播通知实体的数据访问能力

---
+++ src/main/java/com/xrq/xxq/module/notification/mapper/NotificationBroadcastMapper.java
@@ -0,0 +1,12 @@
+package com.xrq.xxq.module.notification.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.notification.entity.NotificationBroadcast;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 广播通知 Mapper。
+ */
+@Mapper
+public interface NotificationBroadcastMapper extends BaseMapper<NotificationBroadcast> {
+}
