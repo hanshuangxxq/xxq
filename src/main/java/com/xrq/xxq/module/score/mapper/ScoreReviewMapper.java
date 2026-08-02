bcc810fb2578e32c8475be4f842533d4b7454cc9
寒霜 <gangziwo620@163.com>
2026-08-02 14:38:53 +0800

    feat(score): 添加成绩复核申请的Mapper接口
    - 新增ScoreReviewMapper接口继承BaseMapper
    - 添加@Mapper注解以支持MyBatis映射
    - 实现成绩复核申请的数据访问层代码
    - 提供基础的CRUD功能操作数据库表

---
+++ src/main/java/com/xrq/xxq/module/score/mapper/ScoreReviewMapper.java
@@ -0,0 +1,12 @@
+package com.xrq.xxq.module.score.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.score.entity.ScoreReview;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 成绩复核申请 Mapper。
+ */
+@Mapper
+public interface ScoreReviewMapper extends BaseMapper<ScoreReview> {
+}
