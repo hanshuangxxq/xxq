9503ed99e31846f368b25990fde5f0f4e210184f
寒霜 <gangziwo620@163.com>
2026-08-02 14:38:31 +0800

    feat(score): 添加成绩记录的 Mapper 接口
    - 创建 ScoreMapper 接口继承 BaseMapper
    - 添加 @Mapper 注解实现 MyBatis 映射器标识
    - 定义成绩记录模块的数据访问层契约
    - 添加接口注释说明用途和功能

---
+++ src/main/java/com/xrq/xxq/module/score/mapper/ScoreMapper.java
@@ -0,0 +1,12 @@
+package com.xrq.xxq.module.score.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.score.entity.Score;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 成绩记录 Mapper。
+ */
+@Mapper
+public interface ScoreMapper extends BaseMapper<Score> {
+}
