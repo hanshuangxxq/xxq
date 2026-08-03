b1493550ca840a5dee5cea4ff18ed79b52a51afd
寒霜 <gangziwo620@163.com>
2026-08-03 15:42:23 +0800

    feat(analysis): 添加EvaluationItem的数据访问接口
    - 新增EvaluationItemMapper接口，继承BaseMapper
    - 使用@Mapper注解标识为MyBatis映射器
    - 支持对EvaluationItem实体的基本数据库操作

---
+++ src/main/java/com/xrq/xxq/module/analysis/mapper/EvaluationItemMapper.java
@@ -0,0 +1,9 @@
+package com.xrq.xxq.module.analysis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.analysis.entity.EvaluationItem;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface EvaluationItemMapper extends BaseMapper<EvaluationItem> {
+}
