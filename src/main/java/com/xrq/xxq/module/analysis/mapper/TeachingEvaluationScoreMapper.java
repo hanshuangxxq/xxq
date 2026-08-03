31e6c6fded6446a0f7f5740b4c9391a0dfbe5d35
寒霜 <gangziwo620@163.com>
2026-08-03 15:48:09 +0800

    feat(analysis): 添加教学评价分数数据访问接口
    - 新增TeachingEvaluationScoreMapper接口
    - 继承MyBatis-Plus的BaseMapper
    - 为教学评价分数实体提供数据库操作支持
    - 添加@Mapper注解以启用MyBatis映射功能

---
+++ src/main/java/com/xrq/xxq/module/analysis/mapper/TeachingEvaluationScoreMapper.java
@@ -0,0 +1,9 @@
+package com.xrq.xxq.module.analysis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.analysis.entity.TeachingEvaluationScore;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TeachingEvaluationScoreMapper extends BaseMapper<TeachingEvaluationScore> {
+}
