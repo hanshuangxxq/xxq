c2be72f61f1a88ae2a127e7c7dad0f7465202115
寒霜 <gangziwo620@163.com>
2026-08-10 13:51:29 +0800

    feat(graduation): 添加毕业作业数据访问接口
    - 创建 GraduationAssignmentMapper 接口继承 BaseMapper
    - 添加 @Mapper 注解以支持 MyBatis 扫描
    - 实现对 GraduationAssignment 实体的基本数据库操作

---
+++ src/main/java/com/xrq/xxq/module/practice/graduation/mapper/GraduationAssignmentMapper.java
@@ -0,0 +1,9 @@
+package com.xrq.xxq.module.practice.graduation.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xrq.xxq.module.practice.graduation.entity.GraduationAssignment;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface GraduationAssignmentMapper extends BaseMapper<GraduationAssignment> {
+}
