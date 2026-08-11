385e0b16d8636b58f4b675c6b24755edf2ef88f3
寒霜 <gangziwo620@163.com>
2026-08-11 22:21:44 +0800

    feat(graduation): 添加毕业成绩Mapper接口
    - 创建GraduationScoreMapper接口继承BaseMapper
    - 使用@Mapper注解标识该接口为MyBatis映射器
    - 关联GraduationScore实体类，实现数据库操作功能

---
+++ src/main/java/com/xrq/xxq/module/practice/graduation/mapper/GraduationScoreMapper.java
@@ -0,0 +1,9 @@
+package com.xrq.xxq.module.practice.graduation.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import com.xrq.xxq.module.practice.graduation.entity.GraduationScore;
+
+@Mapper
+public interface GraduationScoreMapper extends BaseMapper<GraduationScore> {
+}
