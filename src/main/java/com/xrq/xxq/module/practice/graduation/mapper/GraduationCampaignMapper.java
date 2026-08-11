24e2d810f426b246564d943381b5bded904a413e
寒霜 <gangziwo620@163.com>
2026-08-11 22:12:12 +0800

    style(graduation): 调整 GraduationCampaignMapper 导入语句顺序
    - 将 GraduationCampaign 的导入语句调整到 org.apache.ibatis.annotations.Mapper 之后
    - 优化了导入语句的排列顺序以提高代码可读性

--- src/main/java/com/xrq/xxq/module/practice/graduation/mapper/GraduationCampaignMapper.java
+++ src/main/java/com/xrq/xxq/module/practice/graduation/mapper/GraduationCampaignMapper.java
@@ -1,8 +1,8 @@
 package com.xrq.xxq.module.practice.graduation.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xrq.xxq.module.practice.graduation.entity.GraduationCampaign;
 import org.apache.ibatis.annotations.Mapper;
+import com.xrq.xxq.module.practice.graduation.entity.GraduationCampaign;
 
 @Mapper
 public interface GraduationCampaignMapper extends BaseMapper<GraduationCampaign> {
