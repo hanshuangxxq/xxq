ea80509c19934a86fcc6f5aec8be86697b22dec0
寒霜 <gangziwo620@163.com>
2026-08-11 22:12:40 +0800

    style(mapper): 调整 GraduationProposal 导入语句格式
    - 优化了 GraduationProposal 的导入语句顺序
    - 保持代码风格一致性
    - 无功能性修改

--- src/main/java/com/xrq/xxq/module/practice/graduation/mapper/GraduationProposalMapper.java
+++ src/main/java/com/xrq/xxq/module/practice/graduation/mapper/GraduationProposalMapper.java
@@ -1,8 +1,8 @@
 package com.xrq.xxq.module.practice.graduation.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xrq.xxq.module.practice.graduation.entity.GraduationProposal;
 import org.apache.ibatis.annotations.Mapper;
+import com.xrq.xxq.module.practice.graduation.entity.GraduationProposal;
 
 @Mapper
 public interface GraduationProposalMapper extends BaseMapper<GraduationProposal> {
